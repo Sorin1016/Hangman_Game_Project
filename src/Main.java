ab36b4a23b0530d90f489ec8c2b9ffcd80261edb
Sorin <sorinhorcea@gmail.com>
2023-05-07 12:13:23 +0300

    Added random words

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -11,9 +13,13 @@ public class Main {
         // 5. Parcurgem cuvantul si comparam fiecare litera din cuvant cu litera salvata
         // 6. Daca exista litera inlocuim litera de la pozitia literei din cuvant in sirul de stelute
         Scanner sc = new Scanner(System.in);
+        Random rnd = new Random();
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("perfume", "transport", "manager", "woman", "proportion"
+                , "prestige", "vegetable", "environment", "obligation", "example", "liability"));
+        int index = rnd.nextInt(words.size());
         System.out.println("Welcome to HangMan");
             System.out.print("Cuvantul este: ");
-            String hiddenWord = "avion";
+            String hiddenWord = words.get(index);
             char[] stars = new char[hiddenWord.length()];
 //        for (int i = 0; i < stars.length; i++) {
 //            stars[i] = '*';
